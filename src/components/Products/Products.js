8b952ca9766d1a6cf153bdfcf7d6c4cef137b7be
Your Name <you@example.com>
2022-11-18 20:22:59 +0600

    products show in UI

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './Products.css'
-const Products = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+const Products = ({products}) => {
+    const {name,price,img} = products;
+
     return (
-        <div>
-            
+        <div className='card'>
+           <img src={img} alt="product images" />
+           <div className="productInfo">
+            <p className="productName">{name}</p>
+            <div className="cardFooter">
+                <div className="price">${price}</div>
+                <div className="cartDetail">
+                    <span className='cartText'>Add To Cart</span>
+                     <FontAwesomeIcon icon={faShoppingCart} color='white' size='lg'></FontAwesomeIcon> </div>
+            </div>
+           </div>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Products from '../Products/Products';
 import './Shop.css'
 const Shop = () => {
+    const [products,setProducts]=useState([])
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
+    
     return (
-        <div>
-           
+        <div className='shop'>
+           <div className="products">
+            {
+                products.map(product=><Products  products={product} key={product.id}></Products>)
+            }
+            
+           </div>
+           <div className="cart">This is cart section</div>
         </div>
     );
 };
